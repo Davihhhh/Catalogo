5d4ced7833d38dcc49063a793e52432809862b2e
Davihhhh <101269407+Davihhhh@users.noreply.github.com>
2024-02-29 21:59:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,124 +10,103 @@
 	</head>
 	<body>
 		<form action="riceca.php" method="POST">
-            <input type="submit" name="tutte" value="filtra"/>
+            <input type="submit" name="filtrare" value="Filtra"/>
         </form>	
 		<form action="aggiungi.php" method="POST">
-            <input type="submit" name="tutte" value="aggiungi"/>
-        </form>
-		<form action="modifica.php" method="POST">
-            <input type="submit" name="tutte" value="modifica"/>
-        </form>	
-		<form action="elimina.php" method="POST">
-            <input type="submit" name="tutte" value="elimina"/>
-        </form>
+            <input type="submit" name="aggiungere" value="Aggiungi"/>
+        </form>		
 	</body>	
 </html>
-<?php 
-	$servername = "localhost";
-	$username = "root";
-	$password = "root";
-	$database = "catalogo";
-
-	$conn = mysqli_connect($servername, $username, $password, $database);
-	if (!$conn) {
-      	die("Connessione fallita: " . mysqli_connect_error());
-		header("location:index.html");
-		unset($_SESSION);
-		exit();
-    }
-	else
-	{	
-		echo "<h1>Tabelle</h1>";
-		echo "<h2>Case Produttrici</h2>";
-		$sql = "SELECT * FROM case_produttrici";
-		$result = mysqli_query($conn, $sql);
-		if (mysqli_num_rows($result) > 0) 
-		{			
-			echo "<table border=1 style=width:20%>";
-			echo "<tr>";
-			echo "<th>Nome</th>";
-			echo "<th>Data_Fondazione</th>";
-			echo "</tr>";
-			$rows = mysqli_fetch_array($result);
-			while($row = mysqli_fetch_array($result)) 
-			{
-				echo "<tr><td>". $row[0] . " </td><td> " . $row[1] . "</td></tr>";
-			}
-			echo "</table>";
-		} 
-		echo "<br><h2>Sedi</h2>";
-		$sql = "SELECT * FROM sedi";
-		$result = mysqli_query($conn, $sql);
-		if (mysqli_num_rows($result) > 0) 
+<?php 	
+	echo "<h1>Tabelle</h1>";
+	echo "<h2>Case Produttrici</h2>";
+	$sql = "SELECT * FROM case_produttrici";
+	$result = mysqli_query($conn, $sql);
+	if (mysqli_num_rows($result) > 0) 
+	{			
+		echo "<table border=1 style=width:20%>";
+		echo "<tr>";
+		echo "<th>Nome</th>";
+		echo "<th>Data_Fondazione</th>";
+		echo "</tr>";
+		$rows = mysqli_fetch_array($result);
+		while($row = mysqli_fetch_array($result)) 
 		{
-			echo "<table border=1 style=width:40%>";
-			echo "<tr>";
-			echo "<th>Id</th>";
-			echo "<th>Nome</th>";
-			echo "<th>Casa_Produttrice</th>";
-			echo "<th>Indirizzo</th>";
-			echo "</tr>";
-			while($row = mysqli_fetch_array($result)) 
-			{
-				echo "<tr><td>". $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" . $row[3] . "</td></tr>";
-			}
-			echo "</table>";
+			echo "<tr><td>". $row[0] . " </td><td> " . $row[1] . "</td></tr>";
 		}
-		echo "<br><h2>Dispositivi</h2>	";
-		$sql = "SELECT * FROM dispositivi";
-		$result = mysqli_query($conn, $sql);
-		if (mysqli_num_rows($result) > 0) 
+		echo "</table>";
+	} 
+	echo "<br><h2>Sedi</h2>";
+	$sql = "SELECT * FROM sedi";
+	$result = mysqli_query($conn, $sql);
+	if (mysqli_num_rows($result) > 0) 
+	{
+		echo "<table border=1 style=width:40%>";
+		echo "<tr>";
+		echo "<th>Id</th>";
+		echo "<th>Nome</th>";
+		echo "<th>Casa_Produttrice</th>";
+		echo "<th>Indirizzo</th>";
+		echo "</tr>";
+		while($row = mysqli_fetch_array($result)) 
 		{
-			echo "<table border=1 style=width:60%>";
-			echo "<tr>";
-			echo "<th>Id</th>";
-			echo "<th>Nome</th>";
-			echo "<th>Data_Rilascio</th>";
-			echo "<th>Marca</th>";
-			echo "<th>Modello</th>";
-			echo "<th>Casa_Produttrice</th>";
-			echo "</tr>";
-			while($row = mysqli_fetch_array($result)) 
-			{
-				echo "<tr><td>". $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" . $row[3] . "</td><td>" . $row[4] . "</td><td>" . $row[5] . "</td></tr>";
-			}
-			echo "</table>";
-		} 
-		echo "<br><h2>Prodotti</h2>	";
-		$sql = "SELECT * FROM prodotti";
-		$result = mysqli_query($conn, $sql);
-		if (mysqli_num_rows($result) > 0) 
-		{
-			echo "<table border=1 style=width:20%>";
-			echo "<tr>";
-			echo "<th>Numero_Serie</th>";
-			echo "<th>Dispositivo</th>";
-			echo "</tr>";
-			while($row = mysqli_fetch_array($result)) 
-			{
-				echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td></tr>";
-			}
-			echo "</table>";
-		} 
-		echo "<br><h2>Prodotto_Sede</h2>	";
-		$sql = "SELECT * FROM prodotto_sede";
-		$result = mysqli_query($conn, $sql);
-		if (mysqli_num_rows($result) > 0) 
-		{
-			echo "<table border=1 style=width:30%>";
-			echo "<tr>";
-			echo "<th>Id</th>";
-			echo "<th>Prodotto</th>";
-			echo "<th>Sede</th>";
-			echo "</tr>";
-			while($row = mysqli_fetch_array($result)) 
-			{
-				echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td></tr>";
-			}
-			echo "</table>";
-		} 
+			echo "<tr><td>". $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" . $row[3] . "</td></tr>";
+		}
+		echo "</table>";
 	}
+	echo "<br><h2>Dispositivi</h2>	";
+	$sql = "SELECT * FROM dispositivi";
+	$result = mysqli_query($conn, $sql);
+	if (mysqli_num_rows($result) > 0) 
+	{
+		echo "<table border=1 style=width:60%>";
+		echo "<tr>";
+		echo "<th>Id</th>";
+		echo "<th>Nome</th>";
+		echo "<th>Data_Rilascio</th>";
+		echo "<th>Marca</th>";
+		echo "<th>Modello</th>";
+		echo "<th>Casa_Produttrice</th>";
+		echo "</tr>";
+		while($row = mysqli_fetch_array($result)) 
+		{
+			echo "<tr><td>". $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" . $row[3] . "</td><td>" . $row[4] . "</td><td>" . $row[5] . "</td></tr>";
+		}
+		echo "</table>";
+	} 
+	echo "<br><h2>Prodotti</h2>	";
+	$sql = "SELECT * FROM prodotti";
+	$result = mysqli_query($conn, $sql);
+	if (mysqli_num_rows($result) > 0) 
+	{
+		echo "<table border=1 style=width:20%>";
+		echo "<tr>";
+		echo "<th>Numero_Serie</th>";
+		echo "<th>Dispositivo</th>";
+		echo "</tr>";
+		while($row = mysqli_fetch_array($result)) 
+		{
+			echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td></tr>";
+		}
+		echo "</table>";
+	} 
+	echo "<br><h2>Prodotto_Sede</h2>	";
+	$sql = "SELECT * FROM prodotto_sede";
+	$result = mysqli_query($conn, $sql);
+	if (mysqli_num_rows($result) > 0) 
+	{
+		echo "<table border=1 style=width:30%>";
+		echo "<tr>";
+		echo "<th>Id</th>";
+		echo "<th>Prodotto</th>";
+		echo "<th>Sede</th>";
+		echo "</tr>";
+		while($row = mysqli_fetch_array($result)) 
+		{
+			echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td></tr>";
+		}
+		echo "</table>";
+	} 
 
     mysqli_close($conn);
 ?>

d652d78399ae70ca68b0ec8ce3953468a9507685
Davihhhh <101269407+Davihhhh@users.noreply.github.com>
2024-03-01 11:52:48 +0100

    Update accessodatabase.php

--- accessodatabase.php
+++ accessodatabase.php
@@ -1,24 +1,26 @@
 <?php
     session_start();
-
-	$servername = "localhost";
-	$username = $_POST['utente'];
-	$password = $_POST['password'];
-	$database = "catalogo";
-
-	$conn = mysqli_connect($servername, $username, $password, $database);
-	if (!$conn) {
-      	die("Connessione fallita: " . mysqli_connect_error());
-		header("location:index.html");
-		unset($_SESSION);
-		exit();
-    }
-    else 
+    if($_SERVER['REQUEST_METHOD'] == "POST")
     {
-        $_SESSION['operatore'] = $username;
-        $_SESSION['chiave'] = $password;
-        $_SESSION['database'] = $database;
-        $_SESSION['server'] = $servername;
-        header("location:index.php");
+        $servername = "localhost";
+        $username = $_POST['utente'];
+        $password = $_POST['password'];
+        $database = "catalogo";
+
+        $conn = mysqli_connect($servername, $username, $password, $database);
+        if (!$conn) {
+            die("Connessione fallita: " . mysqli_connect_error());
+            header("location:index.html");
+            unset($_SESSION);
+            exit();
+        }
+        else 
+        {
+            $_SESSION['operatore'] = $username;
+            $_SESSION['chiave'] = $password;
+            $_SESSION['database'] = $database;
+            $_SESSION['server'] = $servername;
+            header("location:index.php");
+        }
     }
 ?>

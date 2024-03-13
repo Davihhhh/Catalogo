98b5aea50f332b691c7497efbc84a5df2b68d5d8
Davihhhh <101269407+Davihhhh@users.noreply.github.com>
2024-03-13 11:57:32 +0100

    Update registra.php

--- registra.php
+++ registra.php
@@ -3,8 +3,8 @@
     if($_SERVER['REQUEST_METHOD'] == "POST")
     {
         $servername = "localhost";
-        $username = "basic";
-        $password = "basic";
+        $username = "root";
+        $password = "banana";
         $database = "catalogo";
         
         $conn = new mysqli($servername, $username, $password, $database);
@@ -15,8 +15,8 @@
         else if(isset($_POST['utentenew']) && isset($_POST['passwordnew'])) {
             $new_username = $_POST['utentenew'];
             $new_password = $_POST['passwordnew'];
-        
-            $check_sql = "SELECT * FROM mysql.user WHERE user='$new_username'";
+            $now = date("Y-m-d H:i:s");
+            $check_sql = "SELECT * FROM utenti WHERE Nome='$new_username'";
             $result = mysqli_query($conn, $check_sql);
         
             if (mysqli_num_rows($result) > 0) {
@@ -24,7 +24,7 @@
                 echo '<br><a href="index.html">Torna alla pagina iniziale</a>';
             } 
             else {
-                $sql = "INSERT INTO users (username, password, admin) VALUES ('$new_username', '$new_password', 0)";
+                $sql = "INSERT INTO utenti (username, password, admin) VALUES ('$new_username', '$new_password', '$now')";
         
                 if ($conn->query($sql) === TRUE) {
                     echo "Registrazione effettuata con successo.<br>";

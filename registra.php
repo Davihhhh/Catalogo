e3eb8a36b306a5dadea7be062331db2a1f0ab0a6
Davihhhh <101269407+Davihhhh@users.noreply.github.com>
2024-04-02 21:02:19 +0100

    Update registra.php

--- registra.php
+++ registra.php
@@ -7,37 +7,40 @@
         $password = "banana";
         $database = "catalogo";
         
-		$conn = mysqli_connect($servername, $username, $password, $database);
-        
-        if (!$conn) {
-			die("Connessione fallita: " . mysqli_connect_error());
-			header("location:index.html");
-			unset($_SESSION);
-			exit();
-		}
-        else if(isset($_POST['utentenew']) && isset($_POST['passwordnew'])) {
-            $new_username = $_POST['utentenew'];
-            $new_password = $_POST['passwordnew'];
-            $now = date("Y-m-d H:i:s");
-            $check_sql = "SELECT * FROM utenti WHERE Nome='$new_username'";
-            $result = mysqli_query($conn, $check_sql);
-        
-            if (mysqli_num_rows($result) > 0) {
-                echo "Utente già esistente.";
-                echo '<br><a href="index.html">Torna alla pagina iniziale</a>';
-            } 
-            else {
-                $sql = "INSERT INTO utenti (Nome, Password, Data_Creazione) VALUES ('$new_username', '$new_password', '$now')";
-        
-                if ($conn->query($sql) === TRUE) {
-                    echo "Registrazione effettuata con successo.<br>";
-                    echo '<a href="index.html">Torna alla pagina di login</a>';
-                } else {
-                    echo "Errore durante la registrazione: " . $conn->error;
+        try
+        {
+            $conn = mysqli_connect($servername, $username, $password, $database);
+            
+            if (!$conn) {
+                die("Connessione fallita: " . mysqli_connect_error());
+                header("location:index.html");
+                unset($_SESSION);
+                exit();
+            }
+            else if(isset($_POST['utentenew']) && isset($_POST['passwordnew'])) {
+                $new_username = $_POST['utentenew'];
+                $new_password = $_POST['passwordnew'];
+                $now = date("Y-m-d H:i:s");
+                $check_sql = "SELECT * FROM utenti WHERE Nome='$new_username'";
+                $result = mysqli_query($conn, $check_sql);
+            
+                if (mysqli_num_rows($result) > 0) {
+                    echo "Utente già esistente.";
+                    echo '<br><a href="index.html">Torna alla pagina iniziale</a>';
+                } 
+                else {
+                    $sql = "INSERT INTO utenti (Nome, Password, Data_Creazione) VALUES ('$new_username', '$new_password', '$now')";
+            
+                    if ($conn->query($sql) === TRUE) {
+                        echo "Registrazione effettuata con successo.<br>";
+                        echo '<a href="index.html">Torna alla pagina di login</a>';
+                    } else {
+                        echo "Errore durante la registrazione: " . $conn->error;
+                    }
                 }
             }
-        }
-        
-        $conn->close();
+            
+            $conn->close();
+        } catch (Exception $e) { echo "Registrazione al momento impossibile."; }
     }
 ?>
